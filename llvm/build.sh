a5777354137722be615b540e1865746eae11c540
Carlos Gálvez <carlos.galvez@zenseact.com>
2022-01-02 22:49:29 +0100

    Add LLVM

--- llvm/build.sh
+++ llvm/build.sh
@@ -2,13 +2,13 @@
 set -euxo pipefail
 
 UBUNTU_VERSION=20.04
-GCC_HASH=4f3b1a09d3109f03299a2ac15cfa3059596fcb1d
+LLVM_HASH=104a827ea6de0cbe0f5faef4407552ede31d165c
 
 THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 docker build \
     --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
-    --build-arg GCC_HASH=${GCC_HASH} \
-    --target=gcc \
-    --tag carlosgalvezp/gcc:ubuntu${UBUNTU_VERSION}_${GCC_HASH} \
+    --build-arg LLVM_HASH=${LLVM_HASH} \
+    --target=llvm \
+    --tag carlosgalvezp/llvm:ubuntu${UBUNTU_VERSION}_${LLVM_HASH} \
     ${THIS_DIR}
