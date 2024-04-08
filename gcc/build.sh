847a1267a70c8b3783ed9b373edd4e1165e773c8
Carlos Gálvez <carlos.galvez@zenseact.com>
2024-04-08 21:19:10 +0200

    Bump GCC to latest and use Ubuntu 22.04

--- gcc/build.sh
+++ gcc/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-UBUNTU_VERSION=20.04
-GCC_HASH=f2061b2a9641c2228d4e2d86f19532ad7e93d627
+UBUNTU_VERSION=22.04
+GCC_HASH=f4f7c52472fa59993024e70848559d8bac2167ba
 
 GIT_HASH=$(git rev-parse --short HEAD)
 GCC_TARBALL=gcc-${GCC_HASH:0:7}-ubuntu${UBUNTU_VERSION}-${GIT_HASH}.tar.gz
