8ffe0ef448c9e8611eb2bd25110c10961ddc0827
Carlos Gálvez <carlos.galvez@zenseact.com>
2024-04-08 20:44:17 +0200

    Clean up and use shorter tag name

--- gcc/build_dummy.sh
+++ gcc/build_dummy.sh
@@ -3,8 +3,9 @@ set -euxo pipefail
 
 UBUNTU_VERSION=20.04
 GCC_HASH=f2061b2a9641c2228d4e2d86f19532ad7e93d627
+GCC_HASH_SHORT=${GCC_HASH:0:7}
 GIT_COMMIT=$(git rev-parse --short HEAD)
 
 touch gcc
 
-tar -cvzf gcc-$GCC_HASH-ubuntu$UBUNTU_VERSION-$GIT_COMMIT.tar.gz gcc
+tar -cvzf gcc-$GCC_HASH_SHORT-ubuntu$UBUNTU_VERSION-$GIT_COMMIT.tar.gz gcc
