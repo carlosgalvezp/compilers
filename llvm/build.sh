b5356db83d08d555c0bf32c17daaffd385b42e4f
Carlos Gálvez <carlosgalvezp@gmail.com>
2024-04-09 19:30:47 +0200

    Add LLVM workflow

--- llvm/build.sh
+++ llvm/build.sh
@@ -4,11 +4,21 @@ set -euxo pipefail
 UBUNTU_VERSION=20.04
 LLVM_HASH=104a827ea6de0cbe0f5faef4407552ede31d165c
 
+DATE=$(date +%Y%m%d%H%M%S)
+LLVM_TARBALL=llvm-${DATE}-ubuntu${UBUNTU_VERSION}-${LLVM_HASH:0:7}.tar.gz
+
 THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Build LLVM inside docker image
 docker build \
     --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
     --build-arg LLVM_HASH=${LLVM_HASH} \
-    --target=llvm \
-    --tag carlosgalvezp/llvm:ubuntu${UBUNTU_VERSION}_${LLVM_HASH} \
+    --build-arg LLVM_TARBALL=${LLVM_TARBALL} \
+    --tag llvm:latest \
     ${THIS_DIR}
+
+
+# Take LLVM tarball out of image
+docker create --name tmp llvm:latest
+docker cp tmp:/opt/llvm/$LLVM_TARBALL .
+docker rm tmp
